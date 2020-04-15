ddc4121e5202a3267edeec1352190274fbcb9b8e
风之凌殇 <fzls.zju@gmail.com>
2020-04-15 11:05:51 +0800

    增加打包脚本

---
+++ build.bat
@@ -0,0 +1,7 @@
+:: 使用pyinstaller打包
+pyinstaller.exe --hidden-import pkg_resources.py2_warn  --noconsole -F "再度魔改版史诗装备计算器_by风之凌殇.py"
+
+:: 复制生成的结果后删除临时文件
+COPY /Y "dist\再度魔改版史诗装备计算器_by风之凌殇.exe" "再度魔改版史诗装备计算器_by风之凌殇.exe"
+RMDIR /S /Q "build" "dist" "__pycache__"
+DEL /Q "再度魔改版史诗装备计算器_by风之凌殇.spec"
