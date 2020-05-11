f0e7fc357ab2099ae9e6d40b3148e8a854073a8e
风之凌殇 <fzls.zju@gmail.com>
2020-05-11 19:18:30 +0800

    增加两个换装用的词条，只增加祝福等级，不加其他的

--- 再度魔改版史诗装备计算器_by风之凌殇.py
+++ 再度魔改版史诗装备计算器_by风之凌殇.py
@@ -740,6 +740,18 @@ entry_name_to_indexes = {
             index_buf_bless_lv30,
         ]
     },
+    # (在buff换装中，且与当前身上穿的不一样)所有职业Lv25~30全部技能Lv+X（特性技能除外）
+    "extra_all_job_all_skill_lv_25_30_in_buff_dress_up": {
+        "buf": [
+            index_buf_bless_lv30,
+        ]
+    },
+    # (在buff换装中，且与当前身上穿的不一样)所有职业Lv30~35全部技能Lv+X（特性技能除外）
+    "extra_all_job_all_skill_lv_30_35_in_buff_dress_up": {
+        "buf": [
+            index_buf_bless_lv30,
+        ]
+    },
 }
 
 entry_name_to_name = {
@@ -770,6 +782,8 @@ entry_name_to_name = {
     "extra_all_job_all_skill_lv_20_25": "所有职业Lv20~25全部技能Lv+X（特性技能除外）",
     "extra_all_job_all_skill_lv_25_30": "所有职业Lv25~30全部技能Lv+X（特性技能除外）",
     "extra_all_job_all_skill_lv_30_35": "所有职业Lv30~35全部技能Lv+X（特性技能除外）",
+    "extra_all_job_all_skill_lv_25_30_in_buff_dress_up": "(在buff换装中，且与当前身上穿的不一样)所有职业Lv25~30全部技能Lv+X（特性技能除外）",
+    "extra_all_job_all_skill_lv_30_35_in_buff_dress_up": "(在buff换装中，且与当前身上穿的不一样)所有职业Lv30~35全部技能Lv+X（特性技能除外）",
 }
 
 
