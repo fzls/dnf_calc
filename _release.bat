1edc4535124bcc2015557d012e3de72b2a5ea657
风之凌殇 <fzls.zju@gmail.com>
2020-05-18 03:07:34 +0800

    debug.py在仓库中默认设为调试模式，在发布时替换为非调试模式的脚本

--- _release.bat
+++ _release.bat
@@ -39,7 +39,8 @@ cd "%target_dir%"
 
 :: 移除一些无需发布的文件，以及初始化相关存档
 xcopy preset_clear.XLSX preset.XLSX /Y/B
+xcopy production.py debug.py /Y/B
 RMDIR /S /Q ".git" ".idea" "logs"
-DEL /Q "preset_clear.XLSX" "test.py" “排行结果.xlsx”
+DEL /Q "preset_clear.XLSX" "production.py" "test.py" “排行结果.xlsx”
 ren "魔改后.py__" "魔改后.py"
 ren "calc.py__" "calc.py"
