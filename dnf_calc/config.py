621113574a65002dc76236204ad8cdec3b9720db
风之凌殇 <fzls.zju@gmail.com>
2020-05-20 21:59:49 +0800

    增加日志等级配置

--- dnf_calc/config.py
+++ dnf_calc/config.py
@@ -8,8 +8,9 @@
 # -------------------------------
 
 import toml
+import logging
 
-from dnf_calc import notify_error, logger
+from dnf_calc import notify_error, logger, consoleHandler
 
 g_config = {}
 
@@ -22,9 +23,20 @@ def load_config(config_path = "config.toml"):
     except FileNotFoundError as error:
         notify_error(logger, "未找到{}文件，是否直接在压缩包中打开了？".format(config_path))
         exit(0)
+    set_log_level(g_config)
     logger.info("config loaded")
     logger.debug("config={}".format(g_config))
 
+log_level_map = {
+    "debug": logging.DEBUG,
+    "info": logging.INFO,
+    "warning": logging.WARNING,
+    "error": logging.ERROR,
+    "critical": logging.CRITICAL,
+}
+
+def set_log_level(config):
+    consoleHandler.setLevel(log_level_map[config["log_level"]])
 
 def config():
     return g_config
