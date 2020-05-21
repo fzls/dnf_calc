3f32d038b0c060c81abc1418804f6d7ba3ca4019
风之凌殇 <fzls.zju@gmail.com>
2020-05-21 18:16:20 +0800

    一键安装脚本默认python版本改为3.5.4，支持更多版本的windows

--- _一键安装python并打包.bat
+++ _一键安装python并打包.bat
@@ -4,35 +4,40 @@
 CHCP 65001
 
 :: 设置变量
-SET PYTHON_DOWNLOAD_URL=https://www.python.org/ftp/python/3.8.2/python-3.8.2.exe
-SET PYTHON_VERSION=python-3.8.2
+SET PYTHON_VERSION=3.5.4
+Set PYTHON_PATH_DIR=Python35
+:: 如果当前使用的系统版本比较新（2018年以后的版本），理论上可以使用最新版本的3.8.3，如果比较老，建议使用上面的版本，理论上2015年以后发布的系统都可以安装
+::SET PYTHON_VERSION=3.8.3
+::Set PYTHON_PATH_DIR=Python38
+SET PYTHON_DOWNLOAD_URL=https://www.python.org/ftp/python/%PYTHON_VERSION%/python-%PYTHON_VERSION%.exe
+SET PYTHON_FULL_VERSION=python-%PYTHON_VERSION%
 
 echo.
-echo [提示]: 开始下载 %PYTHON_VERSION%......请耐心等待0-0
+echo [提示]: 开始下载 %PYTHON_FULL_VERSION%......请耐心等待0-0
 echo.
 
 :: 如果已存在则删除
-if exist "%PYTHON_VERSION%.exe" del "%PYTHON_VERSION%.exe"
+if exist "%PYTHON_FULL_VERSION%.exe" del "%PYTHON_FULL_VERSION%.exe"
 
 :: 下载python
 tools\curl.exe --remote-name --remote-header-name %PYTHON_DOWNLOAD_URL%
 
 :: 判断是否下载完成
-if not exist "%PYTHON_VERSION%.exe" cls&color 0c&echo [Error]:Download Python false!...&pause>nul&exit
+if not exist "%PYTHON_FULL_VERSION%.exe" cls&color 0c&echo [Error]:Download Python false!...&pause>nul&exit
 
 echo.
 echo "[提示]: 下载完成，接下来请按照步骤安装python，一路只管点第一个选项就好啦"
 echo.
 
 :: 打开安装程序
-%PYTHON_VERSION%.exe
+%PYTHON_FULL_VERSION%.exe
 
 :: 删除本地安装文件
-del "%PYTHON_VERSION%.exe"&echo [Info]: %PYTHON_VERSION% Installation is complete(OK)!
+del "%PYTHON_FULL_VERSION%.exe"&echo [Info]: %PYTHON_FULL_VERSION% Installation is complete(OK)!
 
 :: 设置path
-set PATH=%PATH%;"%appdata%\..\Local\Programs\Python\Python38-32";"%appdata%\..\Local\Programs\Python\Python38-64";"%appdata%\..\Local\Programs\Python\Python38";"%PROGRAMFILES%\Python38";"%PROGRAMFILES(x86)%\Python38";
-set PATH=%PATH%;"%appdata%\..\Local\Programs\Python\Python38-32\Scripts";"%appdata%\..\Local\Programs\Python\Python38-64\Scripts";"%appdata%\..\Local\Programs\Python\Python38\Scripts";"%PROGRAMFILES%\Python38\Scripts";"%PROGRAMFILES(x86)%\Python38\Scripts";
+set PATH=%PATH%;"%appdata%\..\Local\Programs\Python\%PYTHON_PATH_DIR%-32";"%appdata%\..\Local\Programs\Python\%PYTHON_PATH_DIR%-64";"%appdata%\..\Local\Programs\Python\%PYTHON_PATH_DIR%";"%PROGRAMFILES%\%PYTHON_PATH_DIR%";"%PROGRAMFILES(x86)%\%PYTHON_PATH_DIR%";
+set PATH=%PATH%;"%appdata%\..\Local\Programs\Python\%PYTHON_PATH_DIR%-32\Scripts";"%appdata%\..\Local\Programs\Python\%PYTHON_PATH_DIR%-64\Scripts";"%appdata%\..\Local\Programs\Python\%PYTHON_PATH_DIR%\Scripts";"%PROGRAMFILES%\%PYTHON_PATH_DIR%\Scripts";"%PROGRAMFILES(x86)%\%PYTHON_PATH_DIR%\Scripts";
 
 echo.
 echo "[提示]: python安装完成，接下来开始安装需要用到的一些类库"
