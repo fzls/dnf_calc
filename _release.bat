65a75f83a921f11f4c25789aedbefc0e6ed2e232
风之凌殇 <fzls.zju@gmail.com>
2020-05-29 00:33:53 +0800

    更新发布脚本

--- _release.bat
+++ _release.bat
@@ -20,12 +20,11 @@ call _git_push_remote.bat
 :: 调用构建脚本，打包
 call _build.bat
 
-:: 复制一份各个提示文件到使用说明目录
-for %I in (提示*.txt 注意*.txt 使用说明.txt _常见问题解答*.docx _手动安装运行环境教程*.docx) do xcopy %I "使用说明\"  /Y/B
-
 echo.
 echo [提示]: 构建完成，将结果复制到发布目录
 echo.
+
+echo copy to "%target_dir%"
 
 :: 设置目标目录
 set target_dir=.\..\..\..\downloads\(发布魔改计算器\再度魔改版史诗装备计算器_v%version%_by风之凌殇
@@ -43,10 +42,26 @@ cd "%target_dir%"
 :: 移除一些无需发布的文件，以及初始化相关存档
 xcopy preset_clear.XLSX preset.XLSX /Y/B
 xcopy run_env_release.py dnf_calc\run_env.py /Y/B
-RMDIR /S /Q ".git" ".idea" "logs" "dnf_calc\__pycache__"
-DEL /Q "preset_clear.XLSX" "run_env_release.py" "test.py" “排行结果.xlsx”
-ren "魔改后.py__" "魔改后.py"
-ren "calc.py__" "calc.py"
+
+RMDIR /S /Q ".git"
+RMDIR /S /Q ".idea"
+RMDIR /S /Q "logs"
+RMDIR /S /Q "dnf_calc\__pycache__"
+RMDIR /S /Q "robot"
+RMDIR /S /Q "test"
+
+DEL /Q "preset_clear.XLSX"
+DEL /Q "run_env_release.py"
+DEL /Q "test.py"
+DEL /Q "排行结果.xlsx"
 
 :: re: 配置工具尚未完成，先不发布
 RMDIR /S /Q "dnf_calc_setting_tool"
+
+:: 删除旧版本的这些文件
+cd "使用说明"
+del /Q 提示*.txt 注意*.txt 使用说明.txt _常见问题解答*.docx _手动安装运行环境教程*.docx
+cd ..
+
+:: 复制一份各个提示文件到使用说明目录
+for %I in (提示*.txt 注意*.txt 使用说明.txt _常见问题解答*.docx _手动安装运行环境教程*.docx) do xcopy %I "使用说明\"  /Y/B
