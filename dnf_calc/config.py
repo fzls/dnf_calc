786e95cbe63b9d97039e688546563c0f9768cb97
风之凌殇 <fzls.zju@gmail.com>
2020-05-30 20:17:20 +0800

    增加处理未捕获的异常 fix #25

--- dnf_calc/config.py
+++ dnf_calc/config.py
@@ -291,7 +291,7 @@ def load_config(config_path="config.toml"):
     try:
         raw_config = toml.load(config_path)
         g_config.auto_update_config(raw_config)
-    except FileNotFoundError as error:
+    except Exception as error:
         notify_error(logger, "读取{}文件出错，是否直接在压缩包中打开了？\n具体出错为：".format(config_path, error))
         exit(-1)
 
