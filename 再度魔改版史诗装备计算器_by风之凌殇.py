71fc468fe0ca598cc266b4c6c5b0c30c447e866b
风之凌殇 <fzls.zju@gmail.com>
2020-06-06 00:00:35 +0800

    正确实现reset各个装备的逻辑，保证select_item的key始终为合法装备

--- 再度魔改版史诗装备计算器_by风之凌殇.py
+++ 再度魔改版史诗装备计算器_by风之凌殇.py
@@ -380,10 +380,6 @@ def process_deal(data: CalcData):
         attributes_list = []
         for idx in range(len(for_calc)):
             no_cut = data.opt_one.get(for_calc[idx])
-            if no_cut is None:
-                # hack：目前select是默认初始化时将tg{1101-3336}[0,1]范围的key对应的值都设为0，而百变怪会根据select的值为0来筛选出未选择的集合
-                #  因此在这里如果为None，就是这种情况，直接返回就可以了
-                return
             cut = np.array(no_cut[0:20] + no_cut[22:23] + no_cut[34:35] + no_cut[38:44])  # re：这部分改为外部传入时预先计算
             skiper = multiply_entry(skiper, cut[index_deal_extra_percent_skill_attack_power])
             attributes_list.append(cut)
@@ -915,12 +911,6 @@ def calc():
                 for idx in range(oneone):
                     # 获取该装备的buff属性
                     cut = setget(for_calc[idx])
-                    if cut is None:
-                        # hack：目前select是默认初始化时将tg{1101-3336}[0,1]范围的key对应的值都设为0，而百变怪会根据select的值为0来筛选出未选择的集合
-                        #  因此在这里如果为None，就是这种情况，直接返回就可以了
-                        global count_invalid
-                        count_invalid = count_invalid + 1
-                        return
                     no_cut = np.array(cut)
                     base_array = base_array + no_cut
                     # 更新需要乘算的数据
@@ -3275,18 +3265,36 @@ def update_thread():
 
 
 def reset_all_equips():
-    know_list2 = ['13390150', '22390240', '23390450', '33390750', '21400340', '31400540', '32410650']
-    for equip_index_excep_last_digit in range(1101, 3336):
-        try:
-            select_item['tg{}0'.format(equip_index_excep_last_digit)] = 0
-        except KeyError as error:
-            passss = 1
-        try:
-            select_item['tg{}1'.format(equip_index_excep_last_digit)] = 0
-        except KeyError as error:
-            passss = 1
-    for the_product_of_wisdom_equip_index in know_list2:
+    # 重置装备
+    # 11 上衣  12 裤子   13 头肩 14 腰带 15 鞋子
+    # 21 手镯  22 项链   23 戒指
+    # 31 辅助装备 32 魔法石 33 耳环
+    order_cfg = config().ui.set_equipments_order
+    set_slots = [
+        (1, 15, eval(order_cfg.armor)),  # 防具五件套
+        (16, 19, eval(order_cfg.jewelry)),  # 首饰
+        (20, 23, eval(order_cfg.special_equipment)),  # 特殊装备
+        (24, 27, eval(order_cfg.spare_parts_mid)),  # 散件（中）
+        (28, 31, eval(order_cfg.spare_parts_left)),  # 散件（左）
+        (32, 35, eval(order_cfg.spare_parts_right)),  # 散件（右）
+    ]
+
+    for set_slot in set_slots:
+        set_code_start = set_slot[0]
+        set_code_end = set_slot[1]
+        set_slot_info = set_slot[2]
+        for set_code in range(set_code_start, set_code_end + 1):
+            for idx, val in enumerate(set_slot_info):
+                slot, god = val
+                equip_index = "{0:02}{1:02}{2:1}".format(slot, set_code, god)
+
+                select_item['tg{0}'.format(equip_index)] = 0
+
+    # 重置智慧产物
+    for the_product_of_wisdom_equip_index in ['13390150', '22390240', '23390450', '33390750', '21400340', '31400540', '32410650']:
         select_item['tg{}'.format(the_product_of_wisdom_equip_index)] = 0
+
+    # 状态检查
     check_equipment()
     for set_code in range(101, 136):
         check_set(set_code)
