a854ba14d9fea174463c1627a20a0d2b03fa45c4
风之凌殇 <fzls.zju@gmail.com>
2020-06-28 21:40:23 +0800

    打包脚本整合打包配置工具

--- _build.bat
+++ _build.bat
@@ -11,7 +11,7 @@ python _export_excel_to_txt.py
 
 
 echo.
-echo [提示]: 开始打包
+echo [提示]: 开始打包计算器
 echo.
 
 
@@ -25,5 +25,26 @@ DEL /Q "再度魔改版史诗装备计算器_by风之凌殇.spec"
 
 
 echo.
-echo [提示]: 打包结束
+echo [提示]: 计算器打包结束
+echo.
+
+
+echo.
+echo [提示]: 开始打包配置工具
+echo.
+
+cd dnf_calc_setting_tool_py
+
+:: 使用pyinstaller打包
+pyinstaller.exe --hidden-import pkg_resources.py2_warn --hidden-import PySide2.QtXml --noconsole -F "setting_tool.py"
+
+:: 复制生成的结果后删除临时文件
+COPY /Y "dist\setting_tool.exe" "setting_tool.exe"
+RMDIR /S /Q "build" "dist" "__pycache__"
+DEL /Q "setting_tool.spec"
+
+cd ..
+
+echo.
+echo [提示]: 配置工具打包结束
 echo.
