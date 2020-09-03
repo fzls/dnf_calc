dc18670074c7caab0362e8dbef8f4f54517bbd20
风之凌殇 <fzls.zju@gmail.com>
2020-09-03 21:34:30 +0800

    日志着色

--- dnf_calc/__init__.py
+++ dnf_calc/__init__.py
@@ -12,6 +12,8 @@ import pathlib
 import sys
 from datetime import datetime
 
+import colorlog
+
 from .common import *
 from .const import *
 from .enviroment import *
@@ -21,7 +23,14 @@ from .version import *
 ###########################################################
 #                         logging                         #
 ###########################################################
-logFormatter = logging.Formatter("%(asctime)s %(levelname)-5.5s [%(name)s] %(funcName)s:%(lineno)d: %(message)s")
+fileFmtStr = "%(asctime)s [%(name)s] %(funcName)s:%(lineno)d %(levelname)-5.5s: %(message)s"
+consoleFmtStr = "{}%(asctime)s {}[%(name)s] {}%(funcName)s:%(lineno)d {}%(levelname)-5.5s: %(message)s".format(
+    "%(purple)s",
+    "%(purple)s",
+    "%(purple)s",
+    "%(log_color)s",
+)
+
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 logger.name = "calc"
@@ -37,12 +46,27 @@ process_name = multiprocessing.current_process().name
 if is_debug_mode() or "MainProcess" in process_name:
     time_str = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
     fileHandler = logging.FileHandler("{0}/{1}_{2}_{3}.log".format(log_directory, logger.name, process_name, time_str), encoding="utf-8")
-    fileHandler.setFormatter(logFormatter)
+    fileLogFormatter = logging.Formatter(fileFmtStr)
+    fileHandler.setFormatter(fileLogFormatter)
     fileHandler.setLevel(logging.DEBUG)
     logger.addHandler(fileHandler)
 
+consoleLogFormatter = colorlog.ColoredFormatter(
+    consoleFmtStr,
+    datefmt=None,
+    reset=True,
+    log_colors={
+        'DEBUG': 'cyan',
+        'INFO': 'green',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'red',
+    },
+    secondary_log_colors={},
+    style='%'
+)
 consoleHandler = logging.StreamHandler()
-consoleHandler.setFormatter(logFormatter)
+consoleHandler.setFormatter(consoleLogFormatter)
 consoleHandler.setLevel(logging.INFO)
 logger.addHandler(consoleHandler)
 
